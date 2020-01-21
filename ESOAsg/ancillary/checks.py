ea96961ce3ae4515daeffbb334d510a5ff97aa88
EmAstro <emanuele.paolo.farina@gmail.com>
2020-01-21 12:39:06 +0100

    disk space

--- ESOAsg/ancillary/checks.py
+++ ESOAsg/ancillary/checks.py
@@ -4,16 +4,34 @@ Module that performs basic checks on your machine
 
 import sys
 import shutil
+import numpy as np
 
 # ESOAsg imports
 from ESOAsg import msgs
 from ESOAsg import default
 
-def check_disk_space():
-    """Given a limit in GB
+def check_disk_space(min_disk_space=np.float32(default.get_value('min_disk_space'))):
+    """Given a limit in GB in the variable min_disk_space the macro returns
+    true if there is enough space and rises and error otherwise.
+
+    Parameters
+    ----------
+    check_disk_space : np.float32
+        Size of free space on disk required
+
+    Returns
+    -------
+    enough_space : np.bool
+        True if there is enough space on disk
     """
     total, used, free = shutil.disk_usage("./")
     total = total / (1024**3)
     used  = used / (1024**3)
     free  = free / (1024**3)
-    msgs.info("Your disk shows:  Total: {} GB - Used: {} GB - Free: {} GB".format(total, used, free))
+    msgs.info('Your disk shows: Total: {0:.2f} GB, Used: {0:.2f} GB, Free: {0:.2f} GB'.format(total, used, free))
+    if (free>check_disk_space):
+        enough_space = np.bool(1)
+    else:
+        enough_space = np.bool(0)
+        msgs.error('Not enough space on disk (the limit is set to: {0:.2f} GB'.format(check_disk_space))
+    return enough_space
