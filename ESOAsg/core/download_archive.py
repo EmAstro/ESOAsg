cd900d1b8b24b8c1c5d76c9a40c869488b19ad14
EmAstro <emanuele.paolo.farina@gmail.com>
2020-01-21 16:46:31 +0100

    minor

--- ESOAsg/core/download_archive.py
+++ ESOAsg/core/download_archive.py
@@ -46,12 +46,11 @@ def download(dp_id):
         print(file_name)
         # Given a dp_id of a public file, the link to download it is constructed as follows:
         download_url = "http://archive.eso.org/datalink/links?ID=ivo://eso.org/ID?{}&eso_download=file".format(str(file_name.decode("utf-8")))
-        print(download_url)
         #Files are downloaded in a per-position directory structure.
         # All files matching a given position are store under the directory whose name is the composition of the coordinates (underscore separated).
         # retrieving:
         # last_filepath = file_path
-        urllib.request.urlretrieve(download_url)
+        urllib.request.urlretrieve(download_url, filename=str(file_name.decode("utf-8"))+'.fits')
 
     msgs.info('Downloading data')
     
