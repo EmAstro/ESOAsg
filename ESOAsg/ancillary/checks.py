c3b6522ce2bad9387193762205742ad5d16b24ec
EmAstro <emanuele.paolo.farina@gmail.com>
2020-01-21 12:41:35 +0100

    disk space

--- ESOAsg/ancillary/checks.py
+++ ESOAsg/ancillary/checks.py
@@ -12,7 +12,7 @@ from ESOAsg import default
 
 def check_disk_space(min_disk_space=np.float32(default.get_value('min_disk_space'))):
     """Given a limit in GB in the variable min_disk_space the macro returns
-    true if there is enough space and rises and error otherwise.
+    true if there is enough space and rises an error otherwise.
 
     Parameters
     ----------
@@ -33,5 +33,5 @@ def check_disk_space(min_disk_space=np.float32(default.get_value('min_disk_space
         enough_space = np.bool(1)
     else:
         enough_space = np.bool(0)
-        msgs.error('Not enough space on disk (the limit is set to: {0:.2f} GB'.format(check_disk_space))
+        msgs.error('Not enough space on disk')
     return enough_space
