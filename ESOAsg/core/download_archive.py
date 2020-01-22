2ada6073b9ec624177315b4802695b30ead79d7f
EmAstro <emanuele.paolo.farina@gmail.com>
2020-01-22 12:30:12 +0100

    working on having download nice and neat

--- ESOAsg/core/download_archive.py
+++ ESOAsg/core/download_archive.py
@@ -34,58 +34,64 @@ from ESOAsg import ancillary
 
 from IPython import embed
 
-def download(dp_id, instrument=None):
-    """
-    Parameters
-    ----------
 
-    instrument : str
-        Name of the instrument to query. Default is None, i.e.
-        it will collect all the data
+def download(dp_id, min_disk_space=np.float32(default.get_value('min_disk_space'))):
+    r"""
+    Given a filename in the ADP format, the code download the file from the `ESO archive <http://archive.eso.org>`_
 
-    Returns
-    -------
+    ..note::
+        the dp_id should contain only one file name.
+
+    Args:
+        dp_id (`str`):
+            Data product ID to be downloaded.
+        min_disk_space (`numpy.float`):
+            The file will be downloaded only if there is this amount of space (in Gb) free on the disk.
+            By default is set by the `default.txt` file.
+
+    Returns:
+        This download a fits ADP file with the same name of the input.
     """
+
+    # Check for disk space
+    ancillary.checks.check_disk_space(min_disk_space=min_disk_space)
+
+    # if the file name is in byte, this decode it.
+    print(type(dp_id))
+
     for file_name in dp_id:
         print(file_name)
         # Given a dp_id of a public file, the link to download it is constructed as follows:
-        download_url = "http://archive.eso.org/datalink/links?ID=ivo://eso.org/ID?{}&eso_download=file".format(str(file_name.decode("utf-8")))
-        #Files are downloaded in a per-position directory structure.
+        download_url = "http://archive.eso.org/datalink/links?ID=ivo://eso.org/ID?{}&eso_download=file".format(
+            str(file_name.decode("utf-8")))
+        # Files are downloaded in a per-position directory structure.
         # All files matching a given position are store under the directory whose name is the composition of the coordinates (underscore separated).
         # retrieving:
         # last_filepath = file_path
-        urllib.request.urlretrieve(download_url, filename=str(file_name.decode("utf-8"))+'.fits')
+        urllib.request.urlretrieve(download_url, filename=str(file_name.decode("utf-8")) + '.fits')
 
     msgs.info('Downloading data')
-    
+
     # res['symlink']=''
 
-def query_from_radec(position,
-                     maxrec=default.get_value('maxrec')):
-    r"""
-    Query to the ESO TAP service (link defined in `ESOAsg\default.txt`)
-    for a specific location. The `position` needs to be given
-    as an `astropy.coordinates.SkyCoord` object.
 
-    .. note::
-    
-        This is based on a query that returns different 
+def query_from_radec(position, maxrec=default.get_value('maxrec')):
+    r"""
+    Query to the ESO TAP service (link defined in `ESOAsg\default.txt`) for a specific location. The
+    `position` needs to be given as an `astropy.coordinates.SkyCoord` object.
     
     Args:
         position (`astropy.coordinates.SkyCoord`):
-            Coordinates of the sky you want to query in the format
-            of an astropy SkyCoord object. Note that at the moment
-            it works for one target at the time. For further detail
-            see here: `astropy coordinates <https://docs.astropy.org/en/stable/coordinates/>`_
+            Coordinates of the sky you want to query in the format of an `astropy.coordinates.SkyCoord` object. Note
+            that at the moment it works for one target at the time. For further detail see here:
+            `astropy coordinates <https://docs.astropy.org/en/stable/coordinates/>`_
 
-    maxrec (`numpy.int`):
-        Define the maximum number of file that a single query can
-        return from the ESO archive. You probably never need this.
-        By default is set by the `default.txt` file.
+        maxrec (`numpy.int`):
+            Define the maximum number of file that a single query can return from the ESO archive. You probably never
+            need this. By default is set by the `default.txt` file.
 
-    Returns
-    -------
-    result_from_query
+    Returns:
+        result_from_query
 
     """
 
@@ -96,18 +102,18 @@ def query_from_radec(position,
 
     # ToDo EMA
     # This should be more flexible and take lists/arrays as input
-    if len(position.ra.degree)>1:
+    if len(position.ra.degree) > 1:
         msgs.warning('The position should refer to a single pointing')
         msgs.warning('only the first location is taken into account')
         msgs.working('multi pointing will be available in a future release')
+
     # Converting from array to number
-    RA, Dec = np.float32(position.ra.degree[0]), np.float32(position.dec.degree[0])
+    ra, dec = np.float32(position.ra.degree[0]), np.float32(position.dec.degree[0])
 
     # Define query
     query = """SELECT target_name, dp_id, s_ra, s_dec, t_exptime, em_min, em_max,
             em_min, dataproduct_type, instrument_name, abmaglim, proposal_id 
-            FROM ivoa.ObsCore WHERE obs_release_date < getdate() AND 
-            CONTAINS(POINT('',{},{}), s_region)=1""".format(RA, Dec)
+            FROM ivoa.ObsCore WHERE CONTAINS(POINT('',{},{}), s_region)=1""".format(ra, dec)
     msgs.info('The query is:')
     msgs.info('{}'.format(str(query)))
 
