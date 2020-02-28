0719ae1e82fb525b5a855f43e1317424bf241031
EmAstro <emanuele.paolo.farina@gmail.com>
2020-02-28 16:19:10 +0100

    modheader is working

--- ESOAsg/bin/ASGSpecific/fix_muse_std.py
+++ ESOAsg/bin/ASGSpecific/fix_muse_std.py
@@ -3,15 +3,17 @@
 import argparse
 
 import numpy as np
+import hashlib
 
 from astropy.io import fits
+from astropy.io import ascii
 from astropy.coordinates import name_resolve
 from astropy.coordinates import FK5
 # from astropy import units as u
 
 from ESOAsg import msgs
 from ESOAsg.core import fitsfiles
-# from ESOAsg.core import download_archive
+from ESOAsg.core import download_archive
 
 # from IPython import embed
 
@@ -19,9 +21,10 @@ from ESOAsg.core import fitsfiles
 def parse_arguments():
     parser = argparse.ArgumentParser(
         description=r"""
-        This macro make the spectra of standard stars collected with MUSE compatible 
-        with the Phase3 standard. For more details on the project, please see:
+        This macro make the spectra of standard stars collected with MUSE compatible with the Phase3 standard. For more
+        details on the project, please see Ivanov et al. (2019):
         `https://www.eso.org/sci/publications/messenger/archive/no.178-dec19/messenger-no178-17-18.pdf`_      
+        DOI: 10.18727/0722-6691/5165
 
         This uses ESOAsg version {:s}
         """.format(msgs._version),
@@ -62,21 +65,24 @@ if __name__ == '__main__':
     hdr1 = hdul[1].header
 
     msgs.work('Refactoring data structure.')
-    FLUX = hdul[1].data
-    WAVE = hdr1['CRVAL1'] + (hdr1['CDELT1']*np.arange(0., len(FLUX)))
+    msgs.work('Taking data from the file {}'.format(str(input_fits.replace('.fits', '.dat'))))
+    spectra = ascii.read(input_fits.replace('.fits', '.dat'))
+    WAVE = spectra['col1']
+    FLUX = spectra['col2']
+    ERR = spectra['col3']
 
     # double precision:
-    col_format = 'D'
     col_dtype = np.float64
+    col_format = str(len(np.array(WAVE, dtype=col_dtype)))+'D'
 
     col1 = fits.Column(name='WAVE', format=col_format, unit='angstrom',
-                       array=np.array(WAVE, dtype=col_dtype))
+                       array=[np.array(WAVE, dtype=col_dtype)])
     col2 = fits.Column(name='FLUX', format=col_format, unit='erg.s**(-1).cm**(-2).angstrom**(-1)',
-                       array=np.array(FLUX, dtype=col_dtype))
+                       array=[np.array(FLUX, dtype=col_dtype)])
     col3 = fits.Column(name='ERR', format=col_format, unit='erg.s**(-1).cm**(-2).angstrom**(-1)',
-                       array=np.array(FLUX-FLUX, dtype=col_dtype))
+                       array=[np.array(ERR, dtype=col_dtype)])
 
-    hdul[1] = fits.BinTableHDU.from_columns([col1, col2, col3])
+    hdul[1] = fits.BinTableHDU.from_columns([col1, col2, col3], nrows=1)
 
     msgs.work('Refactoring Header.')
 
@@ -84,6 +90,22 @@ if __name__ == '__main__':
     change1_cards, change1_values, change1_comments = [], [], []
     # Object names
     object_name: str = input_fits.replace('_av.fits', '').split('/')[-1]
+    if object_name == 'HD193281A':
+        begin_name = 'HD193281'
+    elif object_name == 'HD193281B':
+        begin_name = 'HD193281'
+    elif object_name == 'HD193256':
+        begin_name = 'HD193281'
+    else:
+        begin_name = object_name
+    msgs.work('Searching for missing KEYWORDS')
+    fullHeader = ascii.read('PROV.txt')
+    this_object = np.zeros(len(fullHeader['FILE']), dtype=np.bool)
+    for file_name, i in zip(fullHeader['FILE'], range(0, len(fullHeader['FILE']))):
+        this_object[i] = file_name.startswith(begin_name)
+    print(fullHeader['FILE'][this_object].data)
+
+
     # few adjustments
     if object_name.startswith('HD'):
         object_name = object_name.replace('HD', 'HD ')
@@ -103,6 +125,12 @@ if __name__ == '__main__':
     radec = name_resolve.get_icrs_coordinates(object_name).transform_to(FK5(equinox='J2000'))
     msgs.warning('The coordinates of the object are taken from an online archive:')
     msgs.warning('RA={}, Dec={}'.format(radec.ra, radec.dec))
+
+    """
+    result_from_query = download_archive.query_from_radec(radec, radius=600., instrument='MUSE')
+    download_archive.download(result_from_query['dp_id'])
+    """
+
     change1_cards.append('RA')
     change1_values.append(radec.ra.value)
     change1_comments.append('[deg] Spectroscopic target position (J2000.0)')
@@ -128,10 +156,9 @@ if __name__ == '__main__':
     """
 
     # TUCD1
-    msgs.warning("AIR WAVE? <------------------")
     change1_cards.append('TUCD1')
     change1_values.append('em.wl;obs.atmos')
-    change1_comments.append('UCD of field 1')
+    change1_comments.append('Air wavelength')
     # TUTYP1
     change1_cards.append('TUTYP1')
     change1_values.append('spec:Data.SpectralAxis.Value')
@@ -147,7 +174,7 @@ if __name__ == '__main__':
 
     # TUCD2
     change1_cards.append('TUCD2')
-    change1_values.append('phot.flux.density;em.wl;meta.main')
+    change1_values.append('phot.flux.density;em.wl; ;meta.main')
     change1_comments.append('UCD of field 2')
     # TUTYP2
     change1_cards.append('TUTYP2')
@@ -163,13 +190,57 @@ if __name__ == '__main__':
     change1_values.append('spec:Data.FluxAxis.Accuracy.StatError')
     change1_comments.append(' ')
 
+    #SPEC_VAL
+    change1_cards.append('SPEC_VAL')
+    change1_values.append(0.1*(np.nanmax(WAVE)+np.nanmin(WAVE))/2.)
+    change1_comments.append('[nm]')
+
+    #SPEC_BW
+    change1_cards.append('SPEC_BW')
+    change1_values.append(0.1*(np.nanmax(WAVE)-np.nanmin(WAVE)))
+    change1_comments.append('[nm]')
+
+    #VOCLASS
+    change1_cards.append('VOCLASS')
+    change1_values.append('SPECTRUM v1.0')
+    change1_comments.append('VO Data Model')
+
+    #VOPUB
+    change1_cards.append('VOPUB')
+    change1_values.append('ESO/SAF')
+    change1_comments.append(' ')
+
+    #TITLE
+    change1_cards.append('TITLE')
+    change1_values.append(object_name + str(' MUSE spectrum'))
+    change1_comments.append(' ')
+
+    #NELEM
+    change1_cards.append('NELEM')
+    change1_values.append(np.int(len(np.array(WAVE, dtype=col_dtype))))
+    change1_comments.append(' ')
+
+    change1_cards.append('TELAPSE')
+    change1_values.append(86400.*np.float_(np.nanmax(fullHeader['MJD-END'][this_object].data)-
+                                           np.nanmin(fullHeader['MJD-OBS'][this_object].data)))
+    change1_comments.append('[s]')
+
+    change1_cards.append('TMID')
+    change1_values.append(0.5*(np.nanmax(fullHeader['MJD-END'][this_object].data)+np.nanmin(fullHeader['MJD-OBS'][
+                                                                                           this_object].data)))
+    change1_comments.append(' ')
+
+
+    # Diameter of the aperture used to extract the spectra in deg.
     change1_cards.append('APERTURE')
     if object_name == '[B86] 133':
-        change1_values.append(4./3600.)
+        change1_values.append(2.*4./3600.)
     if object_name == 'HD 193256':
-        change1_values.append(4.6/3600.)
+        change1_values.append(2.*4.6/3600.)
+    if object_name == 'HD 193281B':
+        change1_values.append(2.*1.2/3600.)
     else:
-        change1_values.append(6./3600.)
+        change1_values.append(2.*6./3600.)
     change1_comments.append('[deg] Aperture width')
 
     for cards, values, comments in zip(change1_cards, change1_values, change1_comments):
@@ -178,6 +249,22 @@ if __name__ == '__main__':
 
     # Now working on HDU0
     change0_cards, change0_values, change0_comments = [], [], []
+
+    change0_cards.append('MJD-OBS')
+    change0_values.append(np.nanmin(fullHeader['MJD-OBS'][this_object].data))
+    change0_comments.append(' ')
+
+    change0_cards.append('MJD-END')
+    change0_values.append(np.nanmax(fullHeader['MJD-END'][this_object].data))
+    change0_comments.append(' ')
+
+    change0_cards.append('EXPTIME')
+    change0_values.append(np.nansum(fullHeader['TEXPTIME'][this_object].data))
+    change0_comments.append(' ')
+
+    change0_cards.append('TEXPTIME')
+    change0_values.append(np.nansum(fullHeader['TEXPTIME'][this_object].data))
+    change0_comments.append(' ')
 
     change0_cards.append('ORIGIN')
     change0_values.append('ESO-PARANAL')
@@ -216,20 +303,16 @@ if __name__ == '__main__':
     change0_comments.append('Data product category')
 
     change0_cards.append('WAVELMIN')
-    change0_values.append(np.nanmin(WAVE))
-    change0_comments.append('[angstrom] Minimum wavelength')
+    change0_values.append(0.1*np.nanmin(WAVE))
+    change0_comments.append('[nm] Minimum wavelength')
 
     change0_cards.append('WAVELMAX')
-    change0_values.append(np.nanmax(WAVE))
-    change0_comments.append('[angstrom] Maximum wavelength')
-
-    change0_cards.append('SPEC_RES')
-    change0_values.append(2750.)
-    change0_comments.append('Spectral resolution')
+    change0_values.append(0.1*np.nanmax(WAVE))
+    change0_comments.append('[nm] Maximum wavelength')
 
     change0_cards.append('SPEC_BIN')
-    change0_values.append(hdr1['CDELT1'])
-    change0_comments.append('[angstrom] Wavelength bin sizes')
+    change0_values.append(0.1*hdr1['CDELT1'])
+    change0_comments.append('[nm] Wavelength bin sizes')
 
     change0_cards.append('REFERENC')
     change0_values.append('doi:10.1051/0004-6361/201936178')
@@ -239,9 +322,90 @@ if __name__ == '__main__':
     change0_values.append('IFU')
     change0_comments.append('Technique of observation')
 
+    change0_cards.append('PROCSOFT')
+    change0_values.append('muse/2.6')
+    change0_comments.append(' ')
+
+    change0_cards.append('SPECSYS')
+    change0_values.append('BARYCENT')
+    change0_comments.append(' ')
+
+    change0_cards.append('PROG_ID')
+    change0_values.append('099.D-0623(A)')
+    change0_comments.append(' ')
+
+    change0_cards.append('FLUXCAL')
+    change0_values.append('ABSOLUTE')
+    change0_comments.append(' ')
+
     change0_cards.append('EXT_OBJ')
     change0_values.append(False)
     change0_comments.append(' ')
+
+    change0_cards.append('M_EPOCH')
+    change0_values.append(False)
+    change0_comments.append(' ')
+
+    change0_cards.append('CONTNORM')
+    change0_values.append(False)
+    change0_comments.append(' ')
+
+    change0_cards.append('TOT_FLUX')
+    change0_values.append(False)
+    change0_comments.append(' ')
+
+    change0_cards.append('FLUXERR')
+    change0_values.append(-2)
+    change0_comments.append(' ')
+
+    change0_cards.append('SNR')
+    msgs.info('Calculated SNR={}'.format(np.nanmedian(FLUX[FLUX > 0.]/ERR[FLUX > 0.])))
+    change0_values.append(np.nanmedian(FLUX[FLUX > 0.]/ERR[FLUX > 0.]))
+    change0_comments.append(' ')
+
+    change0_cards.append('NCOMBINE')
+    change0_values.append(len(fullHeader['MJD-END'][this_object].data))
+    change0_comments.append(' ')
+
+    change0_cards.append('DATE')
+    change0_values.append(fullHeader['DATE'][this_object].data[0])
+    change0_comments.append(' ')
+
+    #SPEC_RES
+    change0_cards.append('SPEC_RES')
+    change0_values.append(np.int(3000))
+    change0_comments.append('Spectral resolution')
+
+    # add ancillary file
+    ancillary_file = input_fits.replace('_av.fits', '.jpg')
+
+    change0_cards.append('ASSON1')
+    change0_values.append(ancillary_file)
+    change0_comments.append(' ')
+
+    change0_cards.append('ASSOM1')
+    change0_values.append(hashlib.md5(open(ancillary_file, 'rb').read()).hexdigest())
+    change0_comments.append(' ')
+
+    change0_cards.append('ASSOC1')
+    change0_values.append('ANCILLARY.PREVIEW')
+    change0_comments.append(' ')
+
+    # PROVENANCEs
+    INDEX = 1
+    for filename in fullHeader['FILE'][this_object].data:
+        PROV = filename.split("/")[1]
+        change0_cards.append('PROV'+str(INDEX))
+        change0_values.append(PROV)
+        change0_comments.append(' ')
+        INDEX = INDEX+1
+
+    INDEX = 1
+    for OBID in np.unique(np.array(fullHeader['OBID1'][this_object].data)):
+        change0_cards.append('OBID'+str(INDEX))
+        change0_values.append(OBID)
+        change0_comments.append(' ')
+        INDEX = INDEX+1
 
     for cards, values, comments in zip(change0_cards, change0_values, change0_comments):
         msgs.work('HDUL[0], Updating {}'.format(cards))
