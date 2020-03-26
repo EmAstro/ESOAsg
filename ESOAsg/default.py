ead090deee20e319d6700a76062621c3b6962202
EmAstro <emanuele.paolo.farina@gmail.com>
2020-03-26 10:49:42 +0100

    Cleaning up

--- ESOAsg/default.py
+++ ESOAsg/default.py
@@ -2,12 +2,11 @@ r""" Load default values
 """
 
 import pkg_resources
-
 from ESOAsg import msgs
 
 
 class Default:
-    """Set default values ESOAsg
+    r"""Set default values for the ESOAsg module
     """
 
     def __init__(self):
@@ -15,9 +14,9 @@ class Default:
         self._load_from_file()
 
     def _load_from_file(self):
-        """Load default dict with values in default.txt
-        """
+        r"""Load default dict with values in default.txt
 
+        """
         default_file = pkg_resources.resource_filename('ESOAsg', 'default.txt')
         default_list = [line.strip().replace('==', '::')
                         for line in open(default_file) if not line.strip().startswith('#') and line.strip() != '']
@@ -25,19 +24,3 @@ class Default:
         for default in default_list:
             default_quantity, default_value = default.split('::')
             self.default_dict[default_quantity] = default_value
-
-    def get_value(self, card_name):  # written by Ema 05.03.2020
-        r""" Gets the default value from the parameter: card_name
-
-        Args:
-            card_name (`str`):
-
-        Returns:
-            the default value of `card_name`. If not existing an error is raised
-
-        """
-        assert isinstance(card_name, str), '{} not in string format'.format(card_name)
-        if card_name in self.default_dict:
-            return self.default_dict[card_name]
-        else:
-            msgs.error('{} Key not present in the default dictionary'.format(card_name))
