aa1fb65f725e4de9e72bd843688da9e7bc6bc833
EmAstro <emanuele.paolo.farina@gmail.com>
2020-07-08 13:37:28 +0200

    change style

--- doc/conf.py
+++ doc/conf.py
@@ -148,8 +148,8 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'sphinx_rtd_theme'
-html_theme = 'sphinxdoc'
+html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinxdoc'
 #html_theme = 'bizstyle'
 #html_theme = 'alabaster'
 
